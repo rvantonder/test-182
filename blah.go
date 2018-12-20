accd2e86c77177b19a6bcef8d0db50955dc34755
Rijnard van Tonder <rvantonder@gmail.com>
2018-12-20 16:05:27 -0500

    Update blah.go

--- blah.go
+++ blah.go
@@ -9,3 +9,16 @@ func f() {
 func f() {
   return errors.New(Fmt.Sprintf("asdf %s", x))
 }
+// blah
+
+func f() {
+  return errors.New(fmt.Sprintf("asdf %s", x))
+}
+
+
+// xx
+
+
+func f() {
+  return errors.New(fmt.Sprintf("quux %s", x))
+}
